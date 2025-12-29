c6ca99863c3d50aaf4daa4bdf2f63d4bbeb9f932
Jason Davis <jasondavis303@gmail.com>
2025-12-28 22:43:09 -0700

    Added class to get full artwork path

---
+++ DustyPig.TVDB/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DustyPig.TVDB;
+
+public static class Utils
+{
+    public static string GetArtworkPath(string artworkPath)
+    {
+        if (string.IsNullOrWhiteSpace(artworkPath))
+            return null;
+
+        if(artworkPath.StartsWith("http://", StringComparison.OrdinalIgnoreCase))
+            return artworkPath;
+
+        if(artworkPath.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+            return artworkPath;
+
+        if(!artworkPath.StartsWith('/'))
+            artworkPath = '/' + artworkPath;
+
+        return "https://artworks.thetvdb.com" + artworkPath;
+    }
+}
